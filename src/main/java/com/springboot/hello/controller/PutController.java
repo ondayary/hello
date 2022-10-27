9c1af7a5fba9890d6c06c40619298f11be6dad36
ondayary@gmail.com <ondayary@gmail.com>
2022-10-27 14:52:49 +0900

    put에 ResponseEntity 적용

---
+++ src/main/java/com/springboot/hello/controller/PutController.java
@@ -0,0 +1,18 @@
+package com.springboot.hello.controller;
+
+import com.springboot.hello.domain.dto.MemberDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/put-api")
+public class PutController {
+
+    @PutMapping("/member3")
+    public ResponseEntity<MemberDto> putMember(@RequestBody MemberDto memberDto) {
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(memberDto);
+    }
+}
