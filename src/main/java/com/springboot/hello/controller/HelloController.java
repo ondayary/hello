388491be5bcae1c94fde9ad029cc3a4851b2171a
ondayary@gmail.com <ondayary@gmail.com>
2022-10-27 11:28:41 +0900

    RequestParam 실습

--- src/main/java/com/springboot/hello/controller/HelloController.java
+++ src/main/java/com/springboot/hello/controller/HelloController.java
@@ -12,7 +12,6 @@ public class HelloController {
         return "Hello World";
     }
 
-
     @GetMapping(value = "/name")
     public String getName() {
         return "daon";
@@ -26,6 +25,16 @@ public class HelloController {
     @GetMapping(value = "/variable2/{variable}")
     public String getVariable2 (@PathVariable String var) {
         return var;
+    }
+
+
+    // /request1 을 받는 get endpoint만들기
+    @GetMapping(value = "/request1")
+    // RequestParam으로 3개의 값 받기
+    public String getVariable2(@RequestParam String name,
+                               @RequestParam String email,
+                               @RequestParam String organization) {
+        return String.format("%s %s %s", name, email, organization);
     }
 
 }
