be26af184d83ff3f7d8c207ed68013f436291dd9
ondayary@gmail.com <ondayary@gmail.com>
2022-11-01 14:46:05 +0900

    전국 병의원 정보 검색 API 만들기 - getCount() 추가

--- src/main/java/com/springboot/hello/dao/HospitalDao.java
+++ src/main/java/com/springboot/hello/dao/HospitalDao.java
@@ -14,6 +14,12 @@ public class HospitalDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    public int getCount() {
+        String sql = "select count(id) from nation_wide_hospitals;";
+        return this.jdbcTemplate.queryForObject(sql, Integer.class);
+    }
+
+    // add
     // List<Hospital> -- 11만건 Hospital
     public void add(Hospital hospital) {
         String sql = "INSERT INTO `likelion-db`.`nation_wide_hospitals` (`id`, `open_service_name`, `open_local_government_code`, `management_number`, `license_date`, `business_status`, `business_status_code`, `phone`, `full_address`, `road_name_address`, `hospital_name`, `business_type_name`, `healthcare_provider_count`, `patient_room_count`, `total_number_of_beds`, `total_area_size`) " +
@@ -23,6 +29,7 @@ public class HospitalDao {
                              "?,?,?," +
                              "?,?,?," +
                              "?);"; //  16개
+
         this.jdbcTemplate.update(sql,
                 hospital.getId(),
                 hospital.getOpenServiceName(),
